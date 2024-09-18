7fc1695bb8f87ad6044886b3bc1caf6dd2ff3253
Kevon Kibochi <devokrann@gmail.com>
2024-09-18 10:27:18 +0300

    Initial commit

---
+++ types/form.ts
@@ -0,0 +1,19 @@
+export interface typeFormContact {
+	fname: string;
+	lname: string;
+	email: string;
+	phone: string | null;
+	subject: string;
+	message: string;
+}
+
+export interface typeSignUp {
+	email: string;
+	password: string;
+}
+
+export interface typeSignIn {
+	email: string;
+	password: string;
+	remember: boolean;
+}
